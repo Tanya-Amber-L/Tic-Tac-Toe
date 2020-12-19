744ec24e250eb7d8ddc932a63c4cabd2572c6b0e
Tanya Leenders <tanya.leenders@gmail.com>
2020-12-19 11:33:04 +0100

    WIP: began JS functions

--- docs/script.js
+++ docs/script.js
@@ -1,2 +1,32 @@
 // Personal Project: Tic Tac Toe game.
 // Project begins 4/12/20.
+
+//-----variables-----
+
+let game = document.querySelector(".game");
+const cells = [...document.querySelectorAll(".cell")];
+let turn = "X";
+
+
+//-----creating functions & event listeners------
+
+function initiateGame() {
+    board = ["", "", "", "", "", "", "", "", ""];
+};
+
+function writeMark() {
+    board.forEach((mark, index) => {
+        cells[index].textContent = mark;
+        // console.log(mark, index);
+    });
+};
+
+game.addEventListener("click", function() {
+    //WIP
+})
+
+
+//-----calling functions------
+
+initiateGame();
+writeMark();
