5e63a061a341087cc3404a5f4f2f6340f6e0aef3
Tanya Leenders <tanya.leenders@gmail.com>
2021-03-08 21:42:21 +0100

    game finished, modals ok

--- docs/script.js
+++ docs/script.js
@@ -23,56 +23,68 @@ let drawMessage = () => `Game ended in a draw`;
 
 //-----event listeners & document selector------
 document.querySelectorAll(".cell").forEach(cell => cell.addEventListener("click", cellClicked));
-document.querySelector(".start-again-btn").addEventListener("click", restartGame);
+document.querySelectorAll(".start-again-btn").forEach(button => button.addEventListener("click", restartGame));
 
 //-----functions-----
 function cellClicked(clickedCellEvent) {
+    let clickedCell = clickedCellEvent.target; 
+    // this targets the right cell
+    let clickedCellIndex = parseInt(clickedCell.getAttribute("index")); 
+    // get his index, which is is string, so we transform it in a number
 
-    let clickedCell = clickedCellEvent.target; //cibler la bonne case
-    let clickedCellIndex = parseInt(clickedCell.getAttribute("index")); //récuperer son index, qui est un string qu'on tranforme en integer
-
-    if (gameState[clickedCellIndex] === "") { //si la case est vide, gère l'action dans le jeu et check si la aprtie est finie
-
+    if (gameState[clickedCellIndex] === "") { 
+        // if the cell is empty, one function to handle the game interface & variables, one function to check if someone won the game
         cellPlayed(clickedCell, clickedCellIndex);
         resultValidation();
     }
 }
 
 function cellPlayed(clickedCell, clickedCellIndex) {
-    gameState[clickedCellIndex] = turn; //update dans le gamestate que la case est remplie
-    clickedCell.innerHTML = turn;//écris le joueur qui a clické 
+    gameState[clickedCellIndex] = turn; 
+    // update state in "gamestate" constant
+    clickedCell.innerHTML = turn;
+    // update html with correct player 
 }
 
 function resultValidation() {
     let win = false;
     for (let i = 0; i <= 7; i++) {
-        const winCondition = winningConditions[i]; //vérifier toutes les conditions pour gagner
+        // checks all the situations where u can win
+        const winCondition = winningConditions[i]; 
         let a = gameState[winCondition[0]];
         let b = gameState[winCondition[1]];
         let c = gameState[winCondition[2]];
         if (a === '' || b === '' || c === '') {
+            // if one of the cells corresponding to one "win situation" is empty, ignore
             continue;
-        } // si les cases qui correspondent à un schéma gagnant sont vides, ne fait rien
+        } 
         if (a === b && b === c) {
             win = true;
             break
         }
     }
     if (win) {
+        // if there is a win situation, show modal with correct message
         status.innerHTML = winMessage();
+        modal.classList.add("active-modal");
         return;
     }
-    let roundDraw = !gameState.includes(""); //round draw if no winning when the game is full
+    let roundDraw = !gameState.includes(""); 
+    //round draw if no winning when the game is full
     if (roundDraw) {
+        // if there is a draw situation, show madal with correct message
         status.innerHTML = drawMessage();
+        modal.classList.add("active-modal");
         return;
     }
     turnChange();
+    // if nothing has happened here, then no one has won and there are still moves to do, so, function to handle player change
 }
 
 function turnChange() {
     turn = turn === "X" ? "O" : "X";
     if (turn == "X") {
+        // X's turn: adds class to show his turn
         playerO.classList.remove("player-active");
         playerX.classList.add("player-active");
     }
@@ -83,9 +95,14 @@ function turnChange() {
 }
 
 function restartGame() {
-    gameActive = true;
-    currentPlayer = "X";
-    status.innerHTML = "";
+    turn = "X";
+    playerX.classList.add("player-active");
+    playerO.classList.remove("player-active");
+    // reset player X first
     gameState = ["", "", "", "", "", "", "", "", ""];
     document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
+    // reset cells
+    status.innerHTML = "";
+    modal.classList.remove("active-modal");
+    // reset modal/status
 }
